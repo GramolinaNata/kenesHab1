1b28568a64294b206a6cfa080dc1029aaa250688
Damirbull <letcoode@gmail.com>
2025-11-28 16:33:50 +0500

    fix: vite port

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+    server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
