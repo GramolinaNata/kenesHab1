fd17a3a31409a0ceef2232ffa4c57029e6a56045
Damirbull <letcoode@gmail.com>
2025-11-28 17:56:40 +0500

    fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,13 @@ export default defineConfig({
     },
   },
   server: {
-    host: true,
+    host: '0.0.0.0',  // Важно для Docker
     port: 3000,
+    strictPort: true,  // Не менять порт если 3000 занят
+  },
+  preview: {
+    host: '0.0.0.0',  // Для production билда
+    port: 3000,
+    strictPort: true,
   }
 })
